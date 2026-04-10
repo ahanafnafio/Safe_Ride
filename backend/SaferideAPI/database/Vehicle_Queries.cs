a01dc9947b6e6fa81b9abac199b7b85c083d5d99
ahanaf <ahanafnafio@gmail.com>
2026-04-09 23:47:33 -0500

    Added Vehicle and History Schema.sql files and Queries.cs files as well for both in total 4 files

---
+++ backend/SaferideAPI/database/Vehicle_Queries.cs
@@ -0,0 +1,13 @@
+/*namespace Saferide.Database;
+
+public class VehicleQueries()
+{
+    public string LicensePlateLookupQuery() //Check if the vehicle with this license plate exits or not
+    {
+        return @"
+            SELECT vehicleID FROM Vehicle
+            WHERE licensePlate = $licensePlate;        
+        ";
+    }
+}
+I am gonna add more Queries and I am commenting this cause Im not sure if this file is gonna break the code or not*/
